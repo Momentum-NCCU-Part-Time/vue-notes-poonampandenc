c0775705bf673e4556922510f9edfe8a69d8f66f
Poonam Pande <p.pande@earthlink.net>
2023-12-07 17:48:15 -0500

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <script setup>
+const notes = [
+    {
+      "id": 1,
+      "title": "Sample note",
+      "body": "This is the body of my note",
+      "updatedAt": "2018-01-01T00:00:00.000Z"
+    },
+    {
+      "id": 2,
+      "title": "Sample note 2",
+      "body": "This is the body of my note 2",
+      "updatedAt": "2018-01-01T00:00:00.000Z"
+    }
+  ]
 </script>
 
 <template>
   <div>
     <h1>Vue Notes</h1>
-    <!-- your code here -->
+    <ul>
+    <li v-for="note in notes">
+  {{ note.title }} 
+  {{ note.body }}
+</li>
+</ul>
+  </div>
+  <div>
+    <form>
+      <div class="form-example">
+        <label for="title">Note title</label>
+        <input type="text" name="title" id="title" required />
+      </div>
+    </form>
   </div>
 </template>
 
